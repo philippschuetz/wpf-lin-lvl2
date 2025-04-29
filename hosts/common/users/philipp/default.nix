d48e2d382c28ba181639025d5582beba6513d0c5
philippschuetz <mail@philippschuetz.com>
2025-04-29 15:11:56 +0200

    Update user config

--- hosts/common/users/philipp/default.nix
+++ hosts/common/users/philipp/default.nix
@@ -12,28 +12,13 @@ in {
     isNormalUser = true;
     shell = pkgs.zsh;
     extraGroups = ifTheyExist [
-      "audio"
       "network"
-      "video"
       "wheel"
-      "podman"
     ];
 
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 changeme philipp"
     ];
     password = "initial";
-  };
-
-  home-manager = {
-    useGlobalPkgs = true;
-    users."philipp" = {
-      imports = [
-        ../../../../home/philipp/${config.networking.hostName}.nix
-      ];
-    };
-    extraSpecialArgs = {
-      inherit inputs outputs;
-    };
   };
 }
